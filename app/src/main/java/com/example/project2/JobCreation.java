11dc013e34507fccf07991695613659aea1edebe
Tabal <daniel.hansen0516@yahoo.com>
2023-12-04 10:13:31 -0500

    Job Creation activity is complete Added connection to the backend for posting the job Edited the activity to include the necessary inputs for a post

--- app/src/main/java/com/example/project2/JobCreation.java
+++ app/src/main/java/com/example/project2/JobCreation.java
@@ -3,42 +3,57 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class JobCreation extends AppCompatActivity {
 
     ImageView ivNewJobImage;
     Button btnCreateNewJob, btnCancelJobPost;
-    EditText etNewJobPosition, etNewJobPhone, etNewJobEmail, etNewJobDesc;
+    EditText etNewJobTitle, etNewJobPhone, etNewJobEmail, etNewJobDesc, etNewJobSalary, etNewJobType;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_creation);
 
+        queue = Volley.newRequestQueue(this);
+
         ivNewJobImage = findViewById(R.id.ivNewJobImage);
         btnCreateNewJob = findViewById(R.id.btnCreateNewJob);
         btnCancelJobPost = findViewById(R.id.btnCancelJobPost);
-        etNewJobPosition = findViewById(R.id.etNewJobPosition);
+        etNewJobTitle = findViewById(R.id.etNewJobTitle);
         etNewJobPhone = findViewById(R.id.etNewJobPhone);
         etNewJobEmail = findViewById(R.id.etNewJobEmail);
+        etNewJobSalary = findViewById(R.id.etNewJobSalary);
         etNewJobDesc = findViewById(R.id.etNewJobDesc);
+        etNewJobTitle = findViewById(R.id.etJobType);
 
         btnCreateNewJob.setOnClickListener(v -> {
             //Parse contents on each item to verify completeness
             if (checkJobContents()) {
-                //post the job
-                //We can use gson here to create the new job using the api endpoint from the backend
-                //All we would do is create the job object with properties matching the json needed from the endpoint to put into the database
+                UseServer useServer = UseServer.getInstance(this);
+                AtomicReference<String> saveResponse = new AtomicReference<>("");
+                useServer.createPosting(response -> {
+                    Log.d("test","Creating a new job posting: " + response);
+                    saveResponse.set(response);
+                }, 1, etNewJobTitle.getText()+"", etNewJobDesc.getText()+"",
+                        etNewJobSalary.getText()+"", etNewJobType.getText()+"");
             }
         });
     }
 
     boolean checkJobContents() {
-        return !etNewJobPosition.getText().equals("") && !etNewJobPhone.getText().equals("") &&
-                !etNewJobEmail.getText().equals("") && !etNewJobDesc.getText().equals("");
+        return !etNewJobTitle.getText().equals("") && !etNewJobEmail.getText().equals("") &&
+                !etNewJobDesc.getText().equals("") && !etNewJobSalary.getText().equals("") &&
+                !etNewJobType.getText().equals("");
     }
 }
