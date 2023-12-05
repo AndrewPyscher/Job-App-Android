76fa9d1605fbfe797f80b7f3a7b9eb1c4056f763
Tabal <daniel.hansen0516@yahoo.com>
2023-12-05 05:08:06 -0500

    Added permissions check Permissions are checked after sign in --Added proper perms to Android Manifest --Added check to main activity

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView botNavBar;
+
+    public static final int LOCATION_REQUEST_CODE = 111;
+    private static final int NOTIFICATION_REQUEST_CODE = 222 ;
+    private static final String TAG = "PERMS STATUS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
         botNavBar = findViewById(R.id.botNavBar);
 
+        checkPermissions();
+
         botNavBar.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
             if (id == R.id.home) {
                 //do nothing
                 return true;
             } else if (id == R.id.search) {
-                //Use start activity with intents to start that particular activity
+                Intent i = new Intent(this, activity_jobs.class);
+                startActivity(i);
                 return true;
-            } else {
-                return false;
+            } else if (id == R.id.profile) {
+                Intent i = new Intent(this, UserProfile.class);
+                return true;
+            } else if (id == R.id.settings) {
+                Intent i = new Intent(this, Settings.class);
+                return true;
+            } else if (id == R.id.navigation_notifications) {
+                //Implement the navigation to a notification page.
+                //This does not exist yet
             }
-            //Etc etc etc, you can modify this however you want to change its behavior
+            return false;
         });
     }
 
@@ -39,5 +59,21 @@ public class MainActivity extends AppCompatActivity {
         //Set default selected item when in this activity in onStart
         //This will guarantee that we have the correct item selected when the activity starts
         botNavBar.setSelectedItemId(R.id.home);
+    }
+
+    public void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Notification permissions NOT granted, requesting...");
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.POST_NOTIFICATIONS}, NOTIFICATION_REQUEST_CODE);
+        } else {
+            Log.d(TAG, "Notification permissions already granted");
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Location permissions NOT granted, requesting...");
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
+            Log.d(TAG, "Location permissions already granted");
+        }
     }
 }
